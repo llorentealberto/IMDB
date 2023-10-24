7d811404fbbeb5a1af9e870215852e4b81312930
[llorentealberto] <[llorentealberto@hotmal.com]>
2023-10-24 12:43:43 +0200

    ssubo cambios de la clase imdb

---
+++ Proyecto_Final_IMDB/imdb.ts
@@ -0,0 +1,15 @@
+import { Movie } from "./movie";
+
+export class Imdb {
+
+
+    public movies: Movie[];
+
+  constructor(movies: Movie[]) {
+
+    this.movies = movies;
+  }
+
+
+
+}
---
+++ Proyecto_Final_IMDB/testImdb.ts
@@ -0,0 +1,19 @@
+
+import { Imdb } from "./imdb";
+import { Movie } from "./movie";
+
+import { Professional } from "./professional";
+
+// voy a crear tres movie diferentes para luego emterlas en la clase imdb 
+
+const movie1=new Movie("armagedon" , 1998 , "american" ,"accion");
+
+const movie2=new Movie("pesadilla en elm street" , 2015 ,"american" , "terror");
+
+const movie3=new Movie("V de Vendetta" , 1995 , "american" , "thriller");
+
+export let totMovies=[movie1 ,movie2 , movie3];
+
+let pruebaImdb = new Imdb(totMovies);
+
+console.log((pruebaImdb.movies));
